75aa4844658eab3bd2649323178d18dd2b605bb0
ibigbug <xiaobayuwei@gmail.com>
2014-05-17 17:19:26 +0800

    remove unnecessary import

--- src/com/payele/ui/QueryActivity.java
+++ src/com/payele/ui/QueryActivity.java
@@ -8,7 +8,6 @@ import org.achartengine.GraphicalView;
 import org.achartengine.chart.BarChart.Type;
 import org.achartengine.model.CategorySeries;
 import org.achartengine.model.XYMultipleSeriesDataset;
-import org.achartengine.model.XYSeries;
 import org.achartengine.renderer.XYMultipleSeriesRenderer;
 import org.achartengine.renderer.XYSeriesRenderer;
 import org.json.JSONArray;
